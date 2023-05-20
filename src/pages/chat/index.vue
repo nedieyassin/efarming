5393faaa929d802d461702c7f4566cddace41f9c
YassinAgile <ynedie@agilesystems.pa.mw>
2023-05-20 22:36:07 +0200

    init

--- src/pages/chat/index.vue
+++ src/pages/chat/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="px-3">
         <div class="pb-6">
-            <h1 class="text-2xl font-bold text-primary-700">Chat with Agricultural Supervisor</h1>
+            <h1 class="text-2xl font-bold text-primary-700">Chat with Agricultural Advisor</h1>
         </div>
     </div>
 </template>
